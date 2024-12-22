b1880e9086a0bc0cedea3d897dcfc3c54133f7af
Nikhil Inuganti <ninuganti15@gmail.com>
2024-12-22 16:21:21 -0600

    minor changes

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -1,8 +1,7 @@
-"use client";
+"use client"
 
-import Image from "next/image";
-import { Button, Typography } from "@material-tailwind/react";
-
+import Image from "next/image"
+import { Button, Typography } from "@material-tailwind/react"
 
 function Hero() {
   return (
@@ -10,54 +9,23 @@ function Hero() {
       <header className="grid !min-h-[49rem] bg-gray-900 px-8">
         <div className="container mx-auto mt-32 grid h-full w-full grid-cols-1 place-items-center lg:mt-14 lg:grid-cols-2">
           <div className="col-span-1">
-            <Typography variant="h1" color="white" className="mb-4">
-              Your Perfect <br /> Learning App
+            <Typography
+              variant="h1"
+              color="white"
+              className="mb-4">
+              Heal 2 Fit
             </Typography>
             <Typography
               variant="lead"
-              className="mb-7 !text-white md:pr-16 xl:pr-28"
-            >
+              className="mb-7 !text-white md:pr-16 xl:pr-28">
               Our app is here to empower you on your quest for knowledge,
               anytime and anywhere.
             </Typography>
-            <Typography className="mb-4" color="white" variant="h6">
-              Get the app
-            </Typography>
-            <div className="flex flex-col gap-2 md:mb-2 md:w-10/12 md:flex-row">
-              <Button
-                size="lg"
-                color="white"
-                className="flex justify-center items-center gap-3"
-              >
-                <Image
-                  width={256}
-                  height={256}
-                  src="/logos/logo-apple.png"
-                  alt="metamask"
-                  className="w-6 h-6"
-                />
-                App Store
-              </Button>
-              <Button
-                size="lg"
-                color="white"
-                className="flex justify-center items-center gap-3"
-              >
-                <Image
-                  width={256}
-                  height={256}
-                  src="/logos/logo-google.png"
-                  alt="metamask"
-                  className="w-6 h-6"
-                />
-                Google Play
-              </Button>
-            </div>
           </div>
           <Image
             width={470}
             height={576}
-            src="/image/iphones.png"
+            src="https://media.istockphoto.com/id/1390699669/photo/assortment-of-healthy-food-for-clean-eating-flexitarian-mediterranean-diet.jpg?s=612x612&w=0&k=20&c=riJ9OUUAPS2KEDgQh7dOpsbaoDGeRPkq1x4RRVtTucE="
             alt="team work"
             className="col-span-1 my-20 h-full max-h-[30rem] -translate-y-32 md:max-h-[36rem] lg:my-0 lg:ml-auto lg:max-h-[40rem] lg:translate-y-0"
           />
@@ -65,13 +33,15 @@ function Hero() {
       </header>
       <div className="mx-8 lg:mx-16 -mt-24 rounded-xl bg-white p-5 md:p-14 shadow-md">
         <div>
-          <Typography variant="h3" color="blue-gray" className="mb-3">
-            Learning App
+          <Typography
+            variant="h3"
+            color="blue-gray"
+            className="mb-3">
+            Dietition Pavani
           </Typography>
           <Typography
             variant="paragraph"
-            className="font-normal !text-gray-500 lg:w-5/12"
-          >
+            className="font-normal !text-gray-500 lg:w-5/12">
             Download our app to dive into a vast library of courses, tutorials,
             and study materials on a wide range of subjects - from programming
             and language learning to personal development and beyond
@@ -79,6 +49,6 @@ function Hero() {
         </div>
       </div>
     </div>
-  );
+  )
 }
-export default Hero;
+export default Hero
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { Typography, IconButton, Button } from "@material-tailwind/react";
 
-const LINKS = ["About Us", "Careers", "Press", "Blog", "Pricing"];
+const LINKS = ["About Us", "Contact", "Pricing"];
 const CURRENT_YEAR = new Date().getFullYear();
 
 export function Footer() {
@@ -13,15 +13,14 @@ export function Footer() {
           <div className="text-center md:text-left">
             <Typography
               as="a"
-              href="https://www.material-tailwind.com"
-              target="_blank"
               variant="h5"
               color="white"
-              className="mb-4"
-            >
-              Material Tailwind
+              className="mb-4">
+              Dietitian Pavani
             </Typography>
-            <Typography color="white" className="mb-12 font-normal">
+            <Typography
+              color="white"
+              className="mb-12 font-normal">
               The reward for getting on the stage is fame.
             </Typography>
             <ul className="flex flex-wrap items-center justify-center md:justify-start">
@@ -33,85 +32,51 @@ export function Footer() {
                     color="white"
                     className={`py-1 font-medium transition-colors ${
                       idx === 0 ? "pr-3" : "px-3"
-                    }`}
-                  >
+                    }`}>
                     {link}
                   </Typography>
                 </li>
               ))}
             </ul>
           </div>
-          <div className="mt-8 w-full md:mt-0 md:w-auto">
-            <Typography variant="h6" color="white" className="mb-3">
-              Get the app
-            </Typography>
-            <div className="flex flex-col gap-2">
-              <Button
-                color="white"
-                className="flex items-center justify-center"
-              >
-                <Image
-                  width={256}
-                  height={256}
-                  src="/logos/logo-apple.png"
-                  className="-mt-0.5 mr-2 h-6 w-6"
-                  alt="ios"
-                />
-                App Store
-              </Button>
-              <Button
-                color="white"
-                className="flex items-center justify-center"
-              >
-                <Image
-                  width={256}
-                  height={256}
-                  src="/logos/logo-google.png"
-                  className="-mt-0.5 mr-2 h-6 w-6"
-                  alt="ios"
-                />
-                Google Play
-              </Button>
-            </div>
-          </div>
         </div>
         <div className="mt-16 flex flex-wrap items-center justify-center gap-y-4 gap-x-8 border-t border-gray-700 py-7 md:justify-between">
           <Typography
             color="white"
-            className="text-center font-normal opacity-75"
-          >
-            &copy; {CURRENT_YEAR} Made with{" "}
-            <a href="https://www.material-tailwind.com" target="_blank">
-              Material Tailwind
-            </a>{" "}
-            by{" "}
-            <a href="https://www.creative-tim.com" target="_blank">
-              Creative Tim
-            </a>
-            .
+            className="text-center font-normal opacity-75">
+            &copy; {CURRENT_YEAR} Made <a>by Nikhil</a>
           </Typography>
 
           <div className="flex gap-2">
-            <IconButton variant="text" color="white">
-              <i className="fa-brands fa-twitter text-2xl not-italic opacity-75"></i>
+            <IconButton
+              variant="text"
+              color="white"
+              onClick={() =>
+                window.open("https://www.instagram.com/dietitian_pavani/?hl=en")
+              }>
+              <i className="fa-brands fa-instagram text-2xl not-italic opacity-75"></i>
             </IconButton>
-            <IconButton variant="text" color="white">
-              <i className="fa-brands fa-linkedin text-2xl not-italic opacity-75"></i>
+            <IconButton
+              variant="text"
+              color="white"
+              onClick={() =>
+                window.open(
+                  "https://www.facebook.com/profile.php?id=100083484403465"
+                )
+              }>
+              <i className="fa-brands fa-facebbeook text-2xl not-italic opacity-75"></i>
             </IconButton>
-            <IconButton variant="text" color="white">
-              <i className="fa-brands fa-facebook text-2xl not-italic opacity-75"></i>
-            </IconButton>
-            <IconButton variant="text" color="white">
-              <i className="fa-brands fa-github text-2xl not-italic opacity-75"></i>
-            </IconButton>
-            <IconButton variant="text" color="white">
-              <i className="fa-brands fa-dribbble text-2xl not-italic opacity-75"></i>
+            <IconButton
+              variant="text"
+              color="white"
+              onClick={() => window.open("https://www.youtube.com/@dietitian_Pavani")}>
+              <i className="fa-brands fa-youtu text-2xl not-italic opacity-75"></i>
             </IconButton>
           </div>
         </div>
       </div>
     </footer>
-  );
+  )
 }
 
 export default Footer;
